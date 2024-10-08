821c1b056222f7554eed7e8096bc6af980bb58c1
Quentin-Chauvelon <quentin.chauvelon@gmail.com>
2024-10-08 20:46:10 +0200

    Add dark mode support

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -20,7 +20,7 @@ const HeroLink = ({ link, icon }: HeroLinkProps) => {
             <div className='group h-full aspect-square group bg-gradient-to-r bg-[--hero-section-buttons-color] rounded-full p-[0.2rem]'>
                 <a href={link} target='_blank'>
                     <div className='aspect-square flex justify-center items-center bg-[--hero-bg-color] group-hover:bg-transparent rounded-full'>
-                        <i className={"fa fa-brands " + icon + " text-[--hero-section-buttons-color] group-hover:text-white rounded-full"}></i>
+                        <i className={"fa fa-brands " + icon + " text-[--hero-section-buttons-color] group-hover:text-[--white] rounded-full"}></i>
                     </div>
                 </a>
             </div >
