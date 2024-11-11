314a97c2151ca753c237408e20ddc5515b83ea88
Quentin-Chauvelon <quentin.chauvelon@gmail.com>
2024-11-11 18:56:14 +0100

    Add spinner and improve placeholder images
    - Add low-resolution image as placeholder for some images (mainly gifs)
    - Add spinner to all project placeholder images

--- src/components/PlaceholderImage.tsx
+++ src/components/PlaceholderImage.tsx
@@ -2,29 +2,53 @@ import { useState } from 'react';
 
 import placeholderImage from '/projects/placeholder.svg';
 
+import "/src/assets/styles/project.css"
+
 
 type PlaceholderImageProps = {
     src: string
+    srcTmp?: string,
+    displaySpinner?: boolean
     alt: string
     styles: string
     onClickCallback?: () => void
 }
 
-const PlaceholderImage = ({ src, alt, styles, onClickCallback }: PlaceholderImageProps) => {
+const PlaceholderImage = ({ src, srcTmp, displaySpinner = true, alt, styles, onClickCallback }: PlaceholderImageProps) => {
+    const [tmpLoaded, setTmpLoaded] = useState(false);
     const [loaded, setLoaded] = useState(false);
     const [lastSrc, setLastSrc] = useState(src);
 
     if (lastSrc !== src) {
         setLoaded(false);
+        setTmpLoaded(false);
         setLastSrc(src);
     }
 
     const handleOnLoad = () => {
-        setLoaded(true);
+        if (!tmpLoaded) {
+            setTmpLoaded(true);
+        } else {
+            setLoaded(true);
+        }
     }
 
+    const imageSrc = tmpLoaded ? src : (srcTmp ? srcTmp : placeholderImage);
+
+    console.log(tmpLoaded, loaded);
+
+
     return (
-        <img src={loaded ? src : placeholderImage} alt={alt} className={styles} onClick={onClickCallback} onLoad={handleOnLoad} />
+        <>
+            <img src={imageSrc} alt={alt} className={styles} onClick={onClickCallback} onLoad={handleOnLoad} />
+            {
+                displaySpinner && !loaded && (
+                    <div className="absolute flex justify-center items-center w-full h-full top-0 left-0">
+                        <div className="loader w-9 h-9" />
+                    </div>
+                )
+            }
+        </>
     )
 }
 
