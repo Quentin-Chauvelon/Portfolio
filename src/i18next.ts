931dda85bdb81ddfdbcf3fdc9b7cc46311ad1f50
Quentin-Chauvelon <quentin.chauvelon@gmail.com>
2024-11-17 12:09:24 +0100

    Add fallback translations for some files
    The translation, scene and hero json files fail to load sometimes
    (because of their small size?). This commit adds a fallback translation
    to make sure they load properly

--- src/i18next.ts
+++ src/i18next.ts
@@ -2,17 +2,64 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import resourcesToBackend from "i18next-resources-to-backend";
+
+
+const bundledResources = {
+    en: {
+        translation: {},
+        scene: {
+            "instructions-text": "Click on an item outlined in yellow to learn more",
+            "paper-text": "Seeking a Summer 2025 Internship in Software Engineering"
+        },
+        hero: {
+            "title": {
+                "hi": "Hi, I'm",
+                "name": "Quentin"
+            },
+            "view-portfolio": "VIEW PORTFOLIO"
+        }
+    },
+    fr: {
+        translation: {},
+        scene: {
+            "instructions-text": "Cliquez sur un objet entouré en jaune pour en apprendre plus",
+            "paper-text": "A la recherche d'un stage à partir de mai 2025 (de 8 à 16 semaines)"
+        },
+        hero: {
+            "title": {
+                "hi": "Bonjour, je suis",
+                "name": "Quentin"
+            },
+            "view-portfolio": "VOIR LE PORTFOLIO"
+        }
+    }
+};
+
 
 i18n
     .use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        debug: false,
+        debug: true,
         fallbackLng: "en",
         interpolation: {
             escapeValue: false
         },
+        backend: {
+            backends: [
+                Backend,
+                resourcesToBackend(bundledResources)
+            ],
+            backendOptions: [{
+                loadPath: "/locales/{{lng}}/{{ns}}.json"
+            }]
+        },
+        react: {
+            useSuspense: false
+        },
+        supportedLngs: ["en", "fr"],
     });
 
 
